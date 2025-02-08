ef8d175b521582d847dfc5e976b055ce061ae068
FathimaIbrahim <ifathimaibrhm@gmail.com>
2025-02-08 19:22:11 +0530

    group_anagram

--- group_anagram.py
+++ group_anagram.py
@@ -1,3 +1,35 @@
+
+from collections import defaultdict
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        holder = defaultdict(list)
+
+        for items in strs:
+            freq = [0] * 26
+            for c in items:
+                freq[ord(c) - ord('a')] += 1
+
+            holder[tuple(freq)].append(items)
+        return list(holder.values())
+# class Solution:
+#     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+#         holder = defaultdict(list)
+#
+#         for thingy in strs:
+#             countedS = "".join(sorted(thingy))
+#             holder[countedS].append(thingy)
+#         return list(holder.values())
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    cases = [ ["eat","tea","tan","ate","nat","bat"], [""] , ["a"]]
+
+    for item in cases:
+        result = solution.groupAnagrams(item)
+        print(result)
+
+
+
+
 

3132ba3165bdbcc77b05946514236dc133b3eabf
FathimaIbrahim <ifathimaibrhm@gmail.com>
2025-02-08 09:37:38 +0530

    contains_duplicate

---
+++ contains_duplicate.py
@@ -0,0 +1,40 @@
+#brute force approach
+# class Solution:
+#     def containsDuplicate(self, nums: list[int]) -> bool:
+#         for i in range(len(nums) - 1):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] == nums[j]:
+#                     return True
+#
+#         return False
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        collect = set()
+
+        for num in nums:
+            if num in collect:
+                return True
+            collect.add(num)
+        return False
+
+# class Solution:
+#     def containsDuplicate(self, nums: list[int]) -> bool:
+#         nums.sort()
+#
+#         for i in range(1,len(nums)):
+#             if nums[i] == nums[i-1]:
+#                 return True
+#         return False
+
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    nums = [1,4,3,1]
+    print(solution.containsDuplicate(nums))
+
+    nums = [1,2,3,4]
+    print(solution.containsDuplicate(nums))
